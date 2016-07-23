7d2aa5d7bb3d026a697897324539c006b3a173a6
Valeriy Shtaihts <bajiepkasmile@yandex.ru>
2016-07-23 12:21:32 +0300

    commit1

---
+++ app/src/main/java/ru/yamblz/translatetraining/StartFragment.java
@@ -0,0 +1,63 @@
+package ru.yamblz.translatetraining;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StartFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public static StartFragment newInstance() {
+        return new StartFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+
+        initViews(view);
+
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private void initViews(View view) {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+    }
+
+}
