edd7d49a60cd53b8124e07a300b009ae1e08b846
Litun <litunone@gmail.com>
2016-07-23 12:35:09 +0300

    Fragment manager

--- app/src/main/java/ru/yamblz/translatetraining/MainActivity.java
+++ app/src/main/java/ru/yamblz/translatetraining/MainActivity.java
@@ -1,13 +1,32 @@
 package ru.yamblz.translatetraining;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements StartFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment mainFragment = fragmentManager.findFragmentById(R.id.container);
+
+        if (mainFragment == null) {
+            mainFragment = new StartFragment();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.container, mainFragment);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
